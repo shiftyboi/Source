11093c67414b9e187cc36f0ad7516a05d8d43ad8
Harry Merritt <harry.j.merritt@gmail.com>
2026-02-04 12:52:01 +0000

    Implement displaying alerts

--- frames/user.py
+++ frames/user.py
@@ -14,18 +14,23 @@ class fr_user(tk.Frame):
         self.txt_alert = tk.StringVar()
         
         self.lb_alert = tk.Label(self,font=("Arial", 15),textvariable=self.txt_alert) # Label that will display the alert.
-        self.lb_alert.grid(row=0,column=0,sticky="nsew")
+        self.lb_alert.grid(row=0,column=1,sticky="nsew")
 
         self.btn_next = tk.Button(self, font=("Arial", 15), command=lambda: self.next(), text="Next")
-        self.btn_next.grid(row=1,column=0)
+        self.btn_next.grid(row=1,column=2)
     
+        self.btn_back = tk.Button(self, text="← Back", command=lambda: self.controller.show_frame("menu"), font=("Arial", 15))
+        self.btn_back.grid(row=1, column=0)
     
     
     
     
     def refresh(self):
         self.controller.geometry("600x500")
+        
         self.columnconfigure(0, weight=1)
+        self.columnconfigure(1, weight=1)
+        self.columnconfigure(2, weight=1)
 
         self.rowconfigure(0, weight=3)
         self.rowconfigure(1, weight=1)
@@ -39,5 +44,23 @@ class fr_user(tk.Frame):
 
 
 ''')
+        
+        currentalert = self.alerts.fetchone()
 
+        if currentalert:
+        
+            device = self.cur.execute(f'''
+                    SELECT name FROM Assets WHERE id = {currentalert[1]}
+    ''').fetchone()
+            
+            lender = self.cur.execute(f'''
+                    SELECT username FROM Users WHERE id = {currentalert[3]}
+    ''').fetchone()
+
+            
+            
+            self.txt_alert.set(f"{lender[0]} is now lending {device[0]}.")
+        
+        else:
+            self.txt_alert.set("No alerts.")
 
