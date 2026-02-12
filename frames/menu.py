b47dfef8d6f6889f542ef6c6d1935f6f07479574
Harry Merritt <harry.j.merritt@gmail.com>
2026-02-12 10:31:09 +0000

    Implement report generation

--- frames/menu.py
+++ frames/menu.py
@@ -1,4 +1,7 @@
 import tkinter as tk
+import tabulate
+import sqlite3
+import datetime
 
 class fr_menu(tk.Frame):
     def __init__(self, parent, controller):
@@ -6,6 +9,8 @@ class fr_menu(tk.Frame):
         self.controller = controller
         tk.Frame.__init__(self, parent)
         self.name = "menu"
+        
+        self.cur = None
         
         self.var_welcome = tk.StringVar() # Needed to dynamically update the username, so it is set correctly
         
@@ -18,7 +23,8 @@ class fr_menu(tk.Frame):
         self.btn_user = tk.Button(self, text="Alerts",font=("Arial", 15), command=lambda: self.controller.show_frame("user"))
         self.btn_user.grid(row=1, column=1, pady=2, padx=5, sticky="e", ipadx=5)
     
-    
+        self.btn_report = tk.Button(self, text="Generate Report", font=("Arial", 15), command= lambda: self.report())
+        self.btn_report.grid(row=1, column=0)
     
     def refresh(self):
         self.var_welcome.set(f"Welcome {self.controller.user}") # Update username displayed
@@ -30,3 +36,20 @@ class fr_menu(tk.Frame):
         self.rowconfigure(0, weight=1)
         self.rowconfigure(1, weight=1)
         self.rowconfigure(2, weight=1)
+
+        self.cur = self.controller.cur
+
+    def report(self):
+        with open("report.txt", "w") as f:
+            
+            data = self.cur.execute('''
+
+
+                        SELECT Assets.id, Assets.name, Assets.date_installed, Assets."serial number", Users.username, Assets.date_borrowed
+                                    FROM Assets
+                                    JOIN Users ON Assets.user=Users.id
+
+                                    ''')
+            table = tabulate.tabulate(data, headers=["Item ID", "Item name", "Date Installed", "Item Serial Number", "Current user", "Date lent"])
+            f.writelines(f"Report generated on {datetime.datetime.now()} by {self.controller.user} \n \n")
+            f.writelines(table)
